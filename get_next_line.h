d4f814bdee0843ddc9e2e8376effb4e3c4c4af66
Ka Chun Lam <kalam@c3r2s5.42london.com>
2023-12-13 17:51:37 +0000

    change

--- get_next_line.h
+++ get_next_line.h
@@ -6,20 +6,20 @@
 /*   By: kalam <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 20:02:38 by kalam             #+#    #+#             */
-/*   Updated: 2023/12/06 20:04:42 by kalam            ###   ########.fr       */
+/*   Updated: 2023/12/13 16:33:53 by kalam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 10
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
 
-#include <fcntl.h>
-#include <stdlib.h>
-#include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 typedef struct s_list
 {
@@ -27,4 +27,14 @@ typedef struct s_list
 	struct s_list	*next;
 }				t_list;
 
+int			found_newline(t_list *list);
+t_list		*find_last_node(t_list *list);
+char		*get_line(t_list *list);
+void		copy_str(t_list *list, char *str);
+int			len_to_newline(t_list *list);
+void		polish_list(t_list **list);
+char		*get_next_line(int fd);
+void		dealloc(t_list **list, t_list *clean_node, char *buf);
+void		create_list(t_list **list, int fd);
 
+#endif
