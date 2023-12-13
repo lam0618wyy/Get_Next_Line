4cced79ea6fc0f44b84bd49d3c507d0a9f884917
Ka Chun Lam <kalam@c3r2s5.42london.com>
2023-12-13 18:12:58 +0000

    change

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,8 +6,29 @@
 /*   By: kalam <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 20:04:59 by kalam             #+#    #+#             */
-/*   Updated: 2023/12/06 20:05:04 by kalam            ###   ########.fr       */
+/*   Updated: 2023/12/13 18:12:29 by kalam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+#include <stdlib.h>
 
+int		found_newline(t_list *list)
+{
+}
+
+t_list	*find_last_node(t_list *list)
+{
+}
+
+void	copy_str(t_list *list, char *str)
+{
+}
+
+int		len_to_newline(t_list *list)
+{
+}
+/* free memory*/
+void	dealloc(t_list **list, t_list *clean_node, char *buf)
+{
+}
